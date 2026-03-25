918e4c3df0aabae462137c7405e15fb1a08e176b
Guillermo <guillermo.hre@gmail.com>
2026-03-24 21:22:35 -0600

    Add reset script to drop MLB schemas for production and development

--- code/setup/reset.py
+++ code/setup/reset.py
@@ -1,11 +1,11 @@
 print("Eliminar esquemas para MLB")
 
-print("Esquemas producción")
+print("Eliminando esquemas producción")
 spark.sql("DROP DATABASE IF EXISTS mlb_prod_bronze CASCADE")
 spark.sql("DROP DATABASE IF EXISTS mlb_prod_silver CASCADE")
 spark.sql("DROP DATABASE IF EXISTS mlb_prod_gold CASCADE")
 
-print("Esquemas develop")
+print("Eliminando esquemas develop")
 spark.sql("DROP DATABASE IF EXISTS mlb_dev_bronze CASCADE")
 spark.sql("DROP DATABASE IF EXISTS mlb_dev_silver CASCADE")
 spark.sql("DROP DATABASE IF EXISTS mlb_dev_gold CASCADE")
