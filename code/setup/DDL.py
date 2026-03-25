3da9e5f56e746eb8fccc54484a827fd66059ae63
Guillermo-HR <guillermo.hre@gmail.com>
2026-03-25 05:21:01 +0000

    quitar conficionales

--- code/setup/DDL.py
+++ code/setup/DDL.py
@@ -1,50 +1,67 @@
-# Seleccionar entorno de ejecución
-env = 'prod'
-print(f"Running on enviroment: {env}")
-
 # Crear esquemas
-print("Crear esquemas para MLB")
-if env == "prod":
-    print("Esquemas producción")
-    spark.sql("CREATE DATABASE IF NOT EXISTS mlb_prod_bronze")
-    spark.sql("CREATE DATABASE IF NOT EXISTS mlb_prod_silver")
-    spark.sql("CREATE DATABASE IF NOT EXISTS mlb_prod_gold")
-elif env == "dev": 
-    print("Esquemas develop")
-    spark.sql("CREATE DATABASE IF NOT EXISTS mlb_dev_bronze")
-    spark.sql("CREATE DATABASE IF NOT EXISTS mlb_dev_silver")
-    spark.sql("CREATE DATABASE IF NOT EXISTS mlb_dev_gold")
+print("Create schemas")
+print("Schemas prod")
+spark.sql("CREATE DATABASE IF NOT EXISTS mlb_prod_bronze")
+spark.sql("CREATE DATABASE IF NOT EXISTS mlb_prod_silver")
+spark.sql("CREATE DATABASE IF NOT EXISTS mlb_prod_gold")
 
-print("Creacion de esquemas finalizada")
+print("Schemas dev")
+spark.sql("CREATE DATABASE IF NOT EXISTS mlb_dev_bronze")
+spark.sql("CREATE DATABASE IF NOT EXISTS mlb_dev_silver")
+spark.sql("CREATE DATABASE IF NOT EXISTS mlb_dev_gold")
+
+print("Create schemas completed")
 
 # Crear tablas
-print("Crear tablas para MLB")
-if env == "prod":
-    print("Tablas producción")
-elif env == "dev":
-    print("Tablas develop")
-    spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_{env}_bronze.game_schedule (
-            game_pk STRING,
-            home_team STRING,
-            away_team STRING,
-            game_scheduled_time TIMESTAMP,
-            status STRING,
-            ingestion_timestamp TIMESTAMP
-            ) USING DELTA""")
-    spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_{env}_bronze.game_data (
-            game_pk STRING,
-            metadata STRING,
-            live_data STRING,
-            ingestion_timestamp TIMESTAMP
-            ) USING DELTA""")
-    spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_{env}_bronze.failed_game_schedule (
-              response STRING,
-              ingestion_timestamp TIMESTAMP
-              ) USING DELTA""")
-    spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_{env}_bronze.failed_game_data (
-            game_pk STRING,
-            response STRING,
-            ingestion_timestamp TIMESTAMP
-            ) USING DELTA""")
+print("Create tables")
+print("Tables prod")
+spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_prod_bronze.game_schedule (
+        game_pk STRING,
+        home_team STRING,
+        away_team STRING,
+        game_scheduled_time TIMESTAMP,
+        status STRING,
+        ingestion_timestamp TIMESTAMP
+        ) USING DELTA""")
+spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_prod_bronze.game_data (
+        game_pk STRING,
+        metadata STRING,
+        live_data STRING,
+        ingestion_timestamp TIMESTAMP
+        ) USING DELTA""")
+spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_prod_bronze.failed_game_schedule (
+        response STRING,
+        ingestion_timestamp TIMESTAMP
+        ) USING DELTA""")
+spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_prod_bronze.failed_game_data (
+        game_pk STRING,
+        response STRING,
+        ingestion_timestamp TIMESTAMP
+        ) USING DELTA""")
+        
+print("Tables dev")
+spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_dev_bronze.game_schedule (
+        game_pk STRING,
+        home_team STRING,
+        away_team STRING,
+        game_scheduled_time TIMESTAMP,
+        status STRING,
+        ingestion_timestamp TIMESTAMP
+        ) USING DELTA""")
+spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_dev_bronze.game_data (
+        game_pk STRING,
+        metadata STRING,
+        live_data STRING,
+        ingestion_timestamp TIMESTAMP
+        ) USING DELTA""")
+spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_dev_bronze.failed_game_schedule (
+        response STRING,
+        ingestion_timestamp TIMESTAMP
+        ) USING DELTA""")
+spark.sql(f"""CREATE TABLE IF NOT EXISTS mlb_dev_bronze.failed_game_data (
+        game_pk STRING,
+        response STRING,
+        ingestion_timestamp TIMESTAMP
+        ) USING DELTA""")
 
-print("Creacion de tablas finalizada")
+print("Create tables completed")
